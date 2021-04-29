4aef2f5e37cc03e14940445ae9304ca94023dc73
Ahn-Minhyuny <dksalsgus@gmail.com>
2021-04-29 16:39:19 +0900

    update

--- Resource/ssd.py
+++ Resource/ssd.py
@@ -12,112 +12,7 @@ from object_detection.utils import ops as utils_ops
 from object_detection.utils import label_map_util
 from object_detection.utils import visualization_utils as vis_util
 
-# 버전 호환성을 위한 코드
-# patch tf1 into `utils.ops`
-utils_ops.tf = tf.compat.v1
-
-# Patch the location of gfile
-tf.gfile = tf.io.gfile
-
-# 모델 가져오는 함수 
-def load_model():
-    model_name = 'ssd_mobilenet_v1_coco_2017_11_17'
-    base_url = 'http://download.tensorflow.org/models/object_detection/tf2/20200711/'
-    model_file = model_name + '.tar.gz'
-    model_dir = tf.keras.utils.get_file(
-    fname=model_name, 
-    origin=base_url + model_file,
-    untar=True)
-
-    model_dir = pathlib.Path(model_dir)/"saved_model"
-
-    model = tf.saved_model.load(str(model_dir))
-
-    return model
-
-
-
-# 모델에 맞게 이미지를 맞춰주는 함수
-def run_inference_for_single_image(model, image):
-    # 넘파이 어레이로 바꿈
-    # image = np.asarray(image)
-    # The input needs to be a tensor, convert it using `tf.convert_to_tensor`.
-    input_tensor = tf.convert_to_tensor(image)
-    # The model expects a batch of images, so add an axis with `tf.newaxis`.
-    input_tensor = input_tensor[tf.newaxis,...]
-
-    # Run inference
-    model_fn = model.signatures['serving_default']
-    output_dict = model_fn(input_tensor)
-
-    # All outputs are batches tensors.
-    # Convert to numpy arrays, and take index [0] to remove the batch dimension.
-    # We're only interested in the first num_detections.
-    num_detections = int(output_dict.pop('num_detections'))
-    output_dict = {key:value[0, :num_detections].numpy() 
-                    for key,value in output_dict.items()}
-    output_dict['num_detections'] = num_detections
-
-    # detection_classes should be ints.
-    output_dict['detection_classes'] = output_dict['detection_classes'].astype(np.int64)
-    
-    # Handle models with masks:
-    if 'detection_masks' in output_dict:
-        output_dict['detection_masks'] = tf.convert_to_tensor(output_dict['detection_masks'], dtype=tf.float32)
-        output_dict['detection_boxes'] = tf.convert_to_tensor(output_dict['detection_boxes'], dtype=tf.float32)
-        # Reframe the the bbox mask to the image size.
-        detection_masks_reframed = utils_ops.reframe_box_masks_to_image_masks(
-                output_dict['detection_masks'], output_dict['detection_boxes'],
-                image.shape[0], image.shape[1],resize_method='nearest')  
-        detection_masks_reframed = tf.cast(detection_masks_reframed > 0.5,
-                                        tf.uint8)
-        output_dict['detection_masks_reframed'] = detection_masks_reframed.numpy()
-        
-    return output_dict
-
-## 함수 테스트
-# image_np = cv2.imread('data/images/image1.jpg')
-# output_dict = run_inference_for_single_image(detection_model, image_np)
-# print(output_dict)
-
-# 이미지를 보여주는 함수 
-def show_inference(model, image_np):
-    # the array based representation of the image will be used later in order to prepare the
-    # result image with boxes and labels on it.
-    # image_np = cv2.imread(str(image_path))
-    # Actual detection.
-    output_dict = run_inference_for_single_image(model, image_np)
-    PATH_TO_LABELS = 'database/models/research/object_detection/data/mscoco_label_map.pbtxt'
-    category_index = label_map_util.create_category_index_from_labelmap(PATH_TO_LABELS, use_display_name=True)
-    # Visualization of the results of a detection.
-    image_np, display_class_list = vis_util.amh_namelist_chimage(
-        image_np,
-        np.array(output_dict['detection_boxes']),
-        output_dict['detection_classes'],
-        output_dict['detection_scores'],
-        category_index,
-        instance_masks=output_dict.get('detection_masks_reframed',None),
-        use_normalized_coordinates=True,
-        line_thickness=8)
-    # print('what : ',output_dict['detection_classes'])
-    # print('what1 : ',output_dict['detection_scores'])
-
-    
-
-    # print('what:', display_class_list)
-    st.image(image_np, width= 600)
-    return display_class_list
-    # cv2.imshow("result",image_np)
-
-def load_image(image_file):
-    img=Image.open(image_file)
-    return img
-
-
-def image_detection():
-
-    # 모델 불러오기
-
+def ssd_inference():
 
     st.title('SSD image object detetion')
 
@@ -125,27 +20,136 @@ def image_detection():
     ssd_video = open('database/video/ssd.mp4','rb').read()
     st.video(ssd_video)
 
-    st.subheader('이미지파일 업로드')
-    image_file = st. file_uploader('Upload Image', #파일업로드 
-                type= ['png', 'jpg','jpeg']) #업로드 될 수 있는 이미지 파일
-    if image_file is not None :
-        detection_model = load_model()
-        st.write(image_file.name)
-        img = load_image(image_file)
-        pix = np.array(img)
-        display_class_list = show_inference(detection_model, pix)
-        # print(pix)
+    with st.echo():
+        # 버전 호환성을 위한 코드
+        # patch tf1 into `utils.ops`
+        utils_ops.tf = tf.compat.v1
 
-        ## 라벨 불러오기
-        # List of the strings that is used to add correct label for each box.
-        # 라벨 가져오기
-        my_legend = np.full(( len(display_class_list) * 25 ,  300 , 3 ) , 255  , dtype='uint8' )
-        for i,class_name in enumerate(display_class_list) :
-            cv2.putText(my_legend, class_name, (5, (i*25) + 17) , 
-                        cv2.FONT_HERSHEY_TRIPLEX, 0.5, (0, 0, 0) , 1 )
+        # Patch the location of gfile
+        tf.gfile = tf.io.gfile
 
-        st.sidebar.image(my_legend,width=300, caption = 'Detected object')
-        
+        # 모델 가져오는 함수 
+        def load_model():
+            model_name = 'ssd_mobilenet_v1_coco_2017_11_17'
+            base_url = 'http://download.tensorflow.org/models/object_detection/tf2/20200711/'
+            model_file = model_name + '.tar.gz'
+            model_dir = tf.keras.utils.get_file(
+            fname=model_name, 
+            origin=base_url + model_file,
+            untar=True)
+
+            model_dir = pathlib.Path(model_dir)/"saved_model"
+
+            model = tf.saved_model.load(str(model_dir))
+
+            return model
+
+
+
+        # 모델에 맞게 이미지를 맞춰주는 함수
+        def run_inference_for_single_image(model, image):
+            # 넘파이 어레이로 바꿈
+            # image = np.asarray(image)
+            # The input needs to be a tensor, convert it using `tf.convert_to_tensor`.
+            input_tensor = tf.convert_to_tensor(image)
+            # The model expects a batch of images, so add an axis with `tf.newaxis`.
+            input_tensor = input_tensor[tf.newaxis,...]
+
+            # Run inference
+            model_fn = model.signatures['serving_default']
+            output_dict = model_fn(input_tensor)
+
+            # All outputs are batches tensors.
+            # Convert to numpy arrays, and take index [0] to remove the batch dimension.
+            # We're only interested in the first num_detections.
+            num_detections = int(output_dict.pop('num_detections'))
+            output_dict = {key:value[0, :num_detections].numpy() 
+                            for key,value in output_dict.items()}
+            output_dict['num_detections'] = num_detections
+
+            # detection_classes should be ints.
+            output_dict['detection_classes'] = output_dict['detection_classes'].astype(np.int64)
+            
+            # Handle models with masks:
+            if 'detection_masks' in output_dict:
+                output_dict['detection_masks'] = tf.convert_to_tensor(output_dict['detection_masks'], dtype=tf.float32)
+                output_dict['detection_boxes'] = tf.convert_to_tensor(output_dict['detection_boxes'], dtype=tf.float32)
+                # Reframe the the bbox mask to the image size.
+                detection_masks_reframed = utils_ops.reframe_box_masks_to_image_masks(
+                        output_dict['detection_masks'], output_dict['detection_boxes'],
+                        image.shape[0], image.shape[1],resize_method='nearest')  
+                detection_masks_reframed = tf.cast(detection_masks_reframed > 0.5,
+                                                tf.uint8)
+                output_dict['detection_masks_reframed'] = detection_masks_reframed.numpy()
+                
+            return output_dict
+
+        ## 함수 테스트
+        # image_np = cv2.imread('data/images/image1.jpg')
+        # output_dict = run_inference_for_single_image(detection_model, image_np)
+        # print(output_dict)
+
+        # 이미지를 보여주는 함수 
+        def show_inference(model, image_np):
+            # the array based representation of the image will be used later in order to prepare the
+            # result image with boxes and labels on it.
+            # image_np = cv2.imread(str(image_path))
+            # Actual detection.
+            output_dict = run_inference_for_single_image(model, image_np)
+            PATH_TO_LABELS = 'database/models/research/object_detection/data/mscoco_label_map.pbtxt'
+            category_index = label_map_util.create_category_index_from_labelmap(PATH_TO_LABELS, use_display_name=True)
+            # Visualization of the results of a detection.
+            image_np, display_class_list = vis_util.amh_namelist_chimage(
+                image_np,
+                np.array(output_dict['detection_boxes']),
+                output_dict['detection_classes'],
+                output_dict['detection_scores'],
+                category_index,
+                instance_masks=output_dict.get('detection_masks_reframed',None),
+                use_normalized_coordinates=True,
+                line_thickness=8)
+            # print('what : ',output_dict['detection_classes'])
+            # print('what1 : ',output_dict['detection_scores'])
+
+            
+
+            # print('what:', display_class_list)
+            st.image(image_np, width= 600)
+            return display_class_list
+            # cv2.imshow("result",image_np)
+
+        def load_image(image_file):
+            img=Image.open(image_file)
+            return img
+
+
+        def image_detection():
+
+            # 모델 불러오기
+
+
+
+            st.subheader('이미지파일 업로드')
+            image_file = st. file_uploader('Upload Image', #파일업로드 
+                        type= ['png', 'jpg','jpeg']) #업로드 될 수 있는 이미지 파일
+            if image_file is not None :
+                detection_model = load_model()
+                st.write(image_file.name)
+                img = load_image(image_file)
+                pix = np.array(img)
+                display_class_list = show_inference(detection_model, pix)
+                # print(pix)
+
+                ## 라벨 불러오기
+                # List of the strings that is used to add correct label for each box.
+                # 라벨 가져오기
+                my_legend = np.full(( len(display_class_list) * 25 ,  300 , 3 ) , 255  , dtype='uint8' )
+                for i,class_name in enumerate(display_class_list) :
+                    cv2.putText(my_legend, class_name, (5, (i*25) + 17) , 
+                                cv2.FONT_HERSHEY_TRIPLEX, 0.5, (0, 0, 0) , 1 )
+
+                st.sidebar.image(my_legend,width=300, caption = 'Detected object')
+                
 
 
 def video_detection():
--- Resource/yolo.py
+++ Resource/yolo.py
@@ -8,148 +8,147 @@ from PIL import Image
 from database.yolo.model.yolo_model import YOLO
 
 
-
-def load_image(image_file):
-    img=Image.open(image_file)
-    return img
-
-
-def process_image(img):
-    ''' 이미지 리사이즈하고, 차원 화장
-    img: 원본 이미지
-    결과는 (64,64,3)으로 프로세싱한 이미지 반환'''
-    image_org = cv2.resize(img, (416,416), interpolation = cv2.INTER_CUBIC)
-    image_org = np.array(image_org, dtype='float32')
-    image_org = image_org / 255.0
-    image_org = np.expand_dims(image_org, axis = 0 )
-
-    return image_org
-
-def get_classess(file):
-  '''   클래스의 이름을 리스트로 가져온다.   '''
-  with open(file) as f :
-    name_of_class = f.readlines()
-
-  name_of_class = [  class_name.strip() for class_name in name_of_class  ]
-
-  return name_of_class
-
-def box_draw(image, boxes, scores, classes, all_classes):
-    ''' 
-    image : 오리지날 이미지 
-    boxes : object의 박스 데이터, ndarray
-    classes : object의 클래스 정보, ndarray
-    scores : object의 확률, ndarray
-    all_classes : 모든 클래스이 이름
-    drow_box 를 그리는 함수'''
-    class_dic ={}
-    i = 0
-    for box, score, cl in zip(boxes, scores, classes):
-        x, y, w, h = box
-
-        top = max(0, np.floor(x + 0.5).astype(int))
-        left = max(0, np.floor(y + 0.5).astype(int))
-        right = min(image.shape[1], np.floor(x + w + 0.5).astype(int))
-        bottom = min(image.shape[0], np.floor(y + h + 0.5).astype(int))
-
-        cv2.rectangle(image, (top, left), (right, bottom), (255, 0, 0), 2)
-        cv2.putText(image, '{0} {1:.2f}'.format(all_classes[cl], score),
-                    (top, left - 6),
-                    cv2.FONT_HERSHEY_SIMPLEX,
-                    0.6, (0, 0, 255), 1,
-                    cv2.LINE_AA)
-        class_dic[i] = [all_classes[cl], score, box]
-        i += 1
-
-    print('class: {0}, score: {1:.2f}'.format(all_classes[cl], score))
-    print(class_dic)
-    print('box coordinate x,y,w,h: {0}'.format(box))
-
-    print("------------")
-    return class_dic
-
-def detect_image( image, yolo, all_classes):
-    ''' image : 오리지날 이미지
-    yolo : 욜로 모델
-    all_classes : 전체 클라스 이름
-    전체 이미지 리턴'''
-
-    pimage =  process_image(image)
-
-    image_boxes, image_classes, image_scores = yolo.predict(pimage, image.shape)
-
-
-
-    if image_boxes is not None:
-         class_dic = box_draw(image, image_boxes, image_scores, image_classes, all_classes)
-    # print('class:',image_classes)
-    # print('image_scores',image_scores)
-    
-    return image, class_dic
-
-
-
-
-def yolo_detection():
+def yolo_inference():
     st.title('YOLO image object detetion')
-
     st.text('서버의 사양이 낮아 변환 불가로 동영상첨부')
-    ssd_video = open('database/video/yolo.mp4','rb').read()
-    st.video(ssd_video)
+    yolo_video = open('database/video/yolo.mp4','rb').read()
+    st.video(yolo_video)
+    with st.echo():
 
-    st.subheader('이미지파일 업로드')
-    image_file = st.file_uploader('Upload Image', #파일업로드 
-                                type= ['png', 'jpg','jpeg']) #업로드 될 수 있는 이미지 파일
-
-    st.sidebar.text('''object\n물체의 정확도에 대한 기준''')
-    object_threshold = st.sidebar.slider('obj_threshold',min_value = 0.2, max_value=1.0, step = 0.1)
-    st.sidebar.text('''non-maximum-suppression (IOU)\n같은 물체의 겹쳐지는 박스를 제거 하기 위한 기준''')
-    nms_threshold = st.sidebar.slider('nms_threshold',min_value = 0.2, max_value=1.0, step = 0.1)
-
-    if image_file is not None :
-        img = load_image(image_file)
-        # st.image(img, width= 700)
-
-        all_classes = get_classess('database/yolo/data/coco_classes.txt')
-
-        yolo = YOLO(object_threshold,nms_threshold)
-
-        st.sidebar.text("object_thresthod : "+ str(round(object_threshold,1)))
-        st.sidebar.text("nms_thresthod : "+ str(round(nms_threshold,1)))
-
-        image = np.array(img)
-        result_image,  class_dic = detect_image(image, yolo, all_classes)
-        if result_image is None:
-            pass
-        else:
-            return result_image, class_dic
-    else :
-        return 0,0
-def select_object(result_image, class_dic):
-    if result_image is 0:
-        pass
-    else :
-        select_list = {i[0] for i in class_dic.values()}
-        select_class= st.sidebar.selectbox('object', list(select_list))
-
-        print(class_dic)
-        st.sidebar.subheader('Detected object')
-        for i, (class_name, class_score,box)  in class_dic.items():
-            st.sidebar.write('**'+class_name+'**'+ '  '+ str(round(class_score,2)))
+        def load_image(image_file):
+            img=Image.open(image_file)
+            return img
 
 
+        def process_image(img):
+            ''' 이미지 리사이즈하고, 차원 화장
+            img: 원본 이미지
+            결과는 (64,64,3)으로 프로세싱한 이미지 반환'''
+            image_org = cv2.resize(img, (416,416), interpolation = cv2.INTER_CUBIC)
+            image_org = np.array(image_org, dtype='float32')
+            image_org = image_org / 255.0
+            image_org = np.expand_dims(image_org, axis = 0 )
 
-        for i in class_dic.values():
-            if i[0] in select_class :
-                box = i[2]
+            return image_org
+
+        def get_classess(file):
+            '''   클래스의 이름을 리스트로 가져온다.   '''
+            with open(file) as f :
+                name_of_class = f.readlines()
+
+            name_of_class = [  class_name.strip() for class_name in name_of_class  ]
+
+            return name_of_class
+
+        def box_draw(image, boxes, scores, classes, all_classes):
+            ''' 
+            image : 오리지날 이미지 
+            boxes : object의 박스 데이터, ndarray
+            classes : object의 클래스 정보, ndarray
+            scores : object의 확률, ndarray
+            all_classes : 모든 클래스이 이름
+            drow_box 를 그리는 함수'''
+            class_dic ={}
+            i = 0
+            for box, score, cl in zip(boxes, scores, classes):
                 x, y, w, h = box
+
                 top = max(0, np.floor(x + 0.5).astype(int))
                 left = max(0, np.floor(y + 0.5).astype(int))
-                right = min(result_image.shape[1], np.floor(x + w + 0.5).astype(int))
-                bottom = min(result_image.shape[0], np.floor(y + h + 0.5).astype(int))
+                right = min(image.shape[1], np.floor(x + w + 0.5).astype(int))
+                bottom = min(image.shape[0], np.floor(y + h + 0.5).astype(int))
 
-                cv2.rectangle(result_image, (top, left), (right, bottom), (135, 250, 51), 2)
+                cv2.rectangle(image, (top, left), (right, bottom), (255, 0, 0), 2)
+                cv2.putText(image, '{0} {1:.2f}'.format(all_classes[cl], score),
+                            (top, left - 6),
+                            cv2.FONT_HERSHEY_SIMPLEX,
+                            0.6, (0, 0, 255), 1,
+                            cv2.LINE_AA)
+                class_dic[i] = [all_classes[cl], score, box]
+                i += 1
 
-        st.image(result_image, width= 700)
+            print('class: {0}, score: {1:.2f}'.format(all_classes[cl], score))
+            print(class_dic)
+            print('box coordinate x,y,w,h: {0}'.format(box))
+
+            print("------------")
+            return class_dic
+
+        def detect_image( image, yolo, all_classes):
+            ''' image : 오리지날 이미지
+            yolo : 욜로 모델
+            all_classes : 전체 클라스 이름
+            전체 이미지 리턴'''
+
+            pimage =  process_image(image)
+
+            image_boxes, image_classes, image_scores = yolo.predict(pimage, image.shape)
+
+
+
+            if image_boxes is not None:
+                class_dic = box_draw(image, image_boxes, image_scores, image_classes, all_classes)
+            # print('class:',image_classes)
+            # print('image_scores',image_scores)
+            
+            return image, class_dic
+
+
+        def yolo_detection():
+            
+            st.subheader('이미지파일 업로드')
+            image_file = st.file_uploader('Upload Image', #파일업로드 
+                                        type= ['png', 'jpg','jpeg']) #업로드 될 수 있는 이미지 파일
+
+            st.sidebar.text('''object\n물체의 정확도에 대한 기준''')
+            object_threshold = st.sidebar.slider('obj_threshold',min_value = 0.2, max_value=1.0, step = 0.1)
+            st.sidebar.text('''non-maximum-suppression (IOU)\n같은 물체의 겹쳐지는 박스를 제거 하기 위한 기준''')
+            nms_threshold = st.sidebar.slider('nms_threshold',min_value = 0.2, max_value=1.0, step = 0.1)
+
+            if image_file is not None :
+                img = load_image(image_file)
+                # st.image(img, width= 700)
+
+                all_classes = get_classess('database/yolo/data/coco_classes.txt')
+
+                yolo = YOLO(object_threshold,nms_threshold)
+
+                st.sidebar.text("object_thresthod : "+ str(round(object_threshold,1)))
+                st.sidebar.text("nms_thresthod : "+ str(round(nms_threshold,1)))
+
+                image = np.array(img)
+                result_image,  class_dic = detect_image(image, yolo, all_classes)
+                if result_image is None:
+                    pass
+                else:
+                    return result_image, class_dic
+            else :
+                return 0,0
+        def select_object(result_image, class_dic):
+            if result_image is 0:
+                pass
+            else :
+                select_list = {i[0] for i in class_dic.values()}
+                select_class= st.sidebar.selectbox('object', list(select_list))
+
+                print(class_dic)
+                st.sidebar.subheader('Detected object')
+                for i, (class_name, class_score,box)  in class_dic.items():
+                    st.sidebar.write('**'+class_name+'**'+ '  '+ str(round(class_score,2)))
+
+
+
+                for i in class_dic.values():
+                    if i[0] in select_class :
+                        box = i[2]
+                        x, y, w, h = box
+                        top = max(0, np.floor(x + 0.5).astype(int))
+                        left = max(0, np.floor(y + 0.5).astype(int))
+                        right = min(result_image.shape[1], np.floor(x + w + 0.5).astype(int))
+                        bottom = min(result_image.shape[0], np.floor(y + h + 0.5).astype(int))
+
+                        cv2.rectangle(result_image, (top, left), (right, bottom), (135, 250, 51), 2)
+
+                st.image(result_image, width= 700)
 
 
